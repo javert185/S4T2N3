b0a34b94895b52802c620f6976f37e98522946b1
javert185 <124713607+javert185@users.noreply.github.com>
2023-08-12 20:07:49 +0200

    Delete S04T02N03CarapuigMasnouSergi directory

--- S04T02N03CarapuigMasnouSergi/src/main/java/cat/itacademy/barcelonactiva/carapuigmasnou/sergi/s04/t02/n03/S04T02N03CarapuigMasnouSergi/model_repository/RepositoriFruita.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.barcelonactiva.carapuigmasnou.sergi.s04.t02.n03.S04T02N03CarapuigMasnouSergi.model_repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import cat.itacademy.barcelonactiva.carapuigmasnou.sergi.s04.t02.n03.S04T02N03CarapuigMasnouSergi.model_domain.Fruita;
-
-public interface RepositoriFruita extends MongoRepository<Fruita, Long> {
-}
